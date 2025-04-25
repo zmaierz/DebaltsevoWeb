c41fe6905dd545e2d6c5200a09c87bac99a31599
Nikita Semenov <nikitalsemenov00@gmail.com>
2025-04-25 13:46:25 +0000

    Реализация метода добавления инцидента Иммунитета в БД

--- engine/Classes/Database/Database.php
+++ engine/Classes/Database/Database.php
@@ -32,6 +32,20 @@ class Database {
         }
     }
 
+    public function createNewImmunityIncident(?string $type, ?string $name, ?string $data, ?string $description = "", ?string $subdata = ""): bool {
+        $conn = $this->getConn();
+        $actualDate = date("Y-m-d H:i:s");
+
+        $query = "INSERT INTO `immunityIncidents` (`ID`, `type`, `name`, `description`, `subdata`, `data`, `time`) VALUES (NULL, '$type', '$name', '$description', '$subdata', '$data', '$actualDate')";
+
+        try {
+            $conn->query($query);
+        }
+        catch (db_connect_error $ex) {
+
+        }
+    }
+
     public function getDataForMenuWithCategory(?string $categoryName): ?array {
         $out = array();
 
